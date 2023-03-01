c19e64159adac3ba468ef1b2f9d6ed4e6ade6be9
Daniel Wossen <wossendaniel7@gmail.com>
2023-03-01 19:38:08 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App component</h1>
+    </div>
+  );
+}
+// now let's learn about react comps 
+// so the func names has to start with capital letters 
+// we are using jsx that will be compiled to html using  babble
+
+export default App;
+
+// don't forget to export all the comps
+// we probably use them else where
+
